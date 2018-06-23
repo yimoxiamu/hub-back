d273f21e8dd5c75979d033dd494699d0fe30f7b1
yimoxiamu <2959623954@qq.com>
2018-06-23 13:57:07 +0800

    111

---
+++ src/main/java/com/hub/hubback/HubBackApplication.java
@@ -0,0 +1,12 @@
+package com.hub.hubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HubBackApplication.class, args);
+    }
+}
